e27f572619bdca2a608b0cde69ffa110a35b59b3
Akaba_nSchool <orih.9619@outlook.jp>
2020-09-09 10:05:54 +0900

    empathを使用した音声感情分析(Python)まで

---
+++ Wakabayashi/empath/empath.py
@@ -0,0 +1,16 @@
+﻿from poster.encode import multipart_encode, MultipartParam
+from poster.streaminghttp import register_openers
+import urllib2
+
+url="https://api.webempath.net/v2/analyzeWav"
+register_openers()
+items = []
+items.append(MultipartParam('apikey', "sEYnCj27dk2RE_SUF-UDJwqfAbOsphqjDd9nr8XJLvE"))
+items.append(MultipartParam.from_file('wav', "voice2.wav"))
+datagen, headers = multipart_encode(items)
+request = urllib2.Request(url, datagen, headers)
+response = urllib2.urlopen(request)
+if response.getcode() == 200:
+	print(response.read())
+else:
+	print("HTTP status %d" % (response.getcode()))
---
+++ Wakabayashi/empath/empath2.py
@@ -0,0 +1,13 @@
+﻿import requests
+
+url ='https://api.webempath.net/v2/analyzeWav'
+
+apikey = 'sEYnCj27dk2RE_SUF-UDJwqfAbOsphqjDd9nr8XJLvE'
+payload = {'apikey': apikey}
+
+wav = "voice.wav"
+data = open(wav, 'rb')
+file = {'wav': data}
+
+res = requests.post(url, params=payload, files=file)
+print(res.json())
