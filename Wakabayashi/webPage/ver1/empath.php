5dd34be48c2179c024a8bddcc69060a70f4b8df1
Akaba_nSchool <orih.9619@outlook.jp>
2020-09-11 13:25:48 +0900

    音声ファイルのアップロードまで

---
+++ Wakabayashi/webPage/ver1/empath.php
@@ -0,0 +1,29 @@
+﻿<?PHP
+	if(!empty($_FILES['voice']['tmp_name']) && is_uploaded_file($_FILES["voice"]["tmp_name"])) {
+		$uploaddir = '';	// アップロード先ディレクトリ
+		$upload = $uploaddir . basename($_FILES['voice']['name']);	// アップロード済みファイルを参照してファイル名の指定
+		if(move_uploaded_file($_FILES['voice']['tmp_name'], $upload)) {
+			echo "アップロードされたファイルを保存しました";
+		}
+		else {
+			echo "アップロードされたファイルの保存に失敗しました";
+		}
+	}
+	else {
+		echo "ファイルがアップロードされていません";
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>結果表示</title>
+	<link rel="stylesheet" href="CSS/CSS.css" type="text/css">
+</head>
+<body>
+	<h1>結果表示</h1>
+	
+</body>
+</html>
