6123f03f61295fb0bfc14091fc74798f854bdc58
Akaba_nSchool <orih.9619@outlook.jp>
2020-09-23 09:25:54 +0900

    ver1.1 非同期(js)で保存まで ver2/wav1.html ボタンで録音開始、通常ダウンロードまで

---
+++ Wakabayashi/webPage/ver1.1/empath.php
@@ -0,0 +1,40 @@
+﻿<?PHP
+	// 感情表現の配列
+	$emotion = ["エラー", "平常　", "怒り　", "楽しい", "悲しみ", "元気　"];
+
+	$command = 'python empath.py';
+	// Pythonの実行
+	// 例:{'error': 0, 'calm': 22, 'anger': 7, 'joy': 10, 'sorrow': 9, 'energy': 8}
+	exec($command, $output);
+	
+	// PHPで表示しやすい形に整形
+	$output = str_replace('{', "", $output[0]);
+	$output = str_replace('}', "", $output);	// {}の削除
+	$outputArray0 = explode(",", $output);	// カンマで区切る
+	$i = 0;
+	foreach($outputArray0 as $op) {
+		$outputArray1[$i] = explode(":", $outputArray0[$i]);	// コロンで区切る
+		$i++;
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>結果表示</title>
+	<link rel="stylesheet" href="CSS/CSS.css" type="text/css">
+</head>
+<body>
+	<h1>結果表示</h1>
+	<p>
+		<?PHP echo $emotion[0] . ": " . $outputArray1[0][1]; ?><br>
+		<?PHP echo $emotion[1] . ": " . $outputArray1[1][1]; ?><br>
+		<?PHP echo $emotion[2] . ": " . $outputArray1[2][1]; ?><br>
+		<?PHP echo $emotion[3] . ": " . $outputArray1[3][1]; ?><br>
+		<?PHP echo $emotion[4] . ": " . $outputArray1[4][1]; ?><br>
+		<?PHP echo $emotion[5] . ": " . $outputArray1[5][1]; ?>
+	</p>
+</body>
+</html>
---
+++ Wakabayashi/webPage/ver1.1/file_send.js
@@ -0,0 +1,31 @@
+﻿// ページ読込完了後にボタンにclickイベントを登録する
+window.addEventListener("load", function(){
+	document.getElementById("wav_send").addEventListener("click", function(){
+		// FoemDataオブジェクトに要素セレクタを渡して宣言する
+		var formDatas = document.getElementById("wav_form");
+		var mixedDatas = new FormData(formDatas);
+
+		// appendメソッドでキーとデータの組をセットする
+		// append("キー(FORMで云うところのname属性値)",  "データ")でデータをセットできる
+		// appendではデータは追加となる
+		mixedDatas.append("filename",  "voice.wav");
+		//mixedDatas.append("filesize",  "10,154B");
+
+		// XHRの宣言
+		var XHR = new XMLHttpRequest();
+
+		// openメソッドにPOSTを指定して送信先のURLを指定します
+		XHR.open("POST", "file_save.php", true);
+
+		// sendメソッドにデータを渡して送信を実行する
+		XHR.send(mixedDatas);
+
+		// サーバの応答をonreadystatechangeイベントで検出して正常終了したらデータを取得する
+		XHR.onreadystatechange = function(){
+			if(XHR.readyState == 4 && XHR.status == 200){
+				// POST送信した結果を表示する
+				document.getElementById("result").innerHTML = "送信成功";
+			}
+		};
+	} ,false);
+}, false);
